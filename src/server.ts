0a9fb607a7c9a9e25a22ca76f2d8c27b1d0809ff
elias fernandes <eliasfernandescout@gmail.com>
2021-10-17 23:03:06 -0300

    add prettier to project

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,11 @@ import express from "express";
 const app = express();
 require("dotenv").config();
 
-app.get('/', (request, response) => {
+app.get("/", (request, response) => {
     response.json({ message: "Hello App" });
 });
 
-app.post("/", (request, response) => {});
+app.post('/', (request, response) => {});
 
 app.listen(process.env.PORT, () => {
     console.log("🟠 SERVER STARTED ON PORT 5002");
