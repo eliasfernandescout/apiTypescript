38797010da0ca68669fbe51051865f0a70de5ba9
elias fernandes <eliasfernandescout@gmail.com>
2021-10-18 13:06:03 -0300

    feat: create categories route

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,12 @@
 import express, { response } from 'express';
 
+import { categoriesRoutes } from './routes/categories.routes';
+
 const app = express();
 app.use(express.json());
 // require('dotenv').config();
 
-app.get('/', (request, response) => {
-    response.json({ message: 'Hello App' });
-});
-
-app.post('/courses', (request, response) => {
-    const { name } = request.body;
-    return response.json({ name });
-});
+app.use(categoriesRoutes);
 
 app.listen(5002, () => {
     console.log('🔶SERVER STARTED ON PORT 5002');
