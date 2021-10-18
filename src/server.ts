46c5b9f59444c9e23eda70b126f6dfdc0c0dd669
elias fernandes <eliasfernandescout@gmail.com>
2021-10-18 00:37:04 -0300

    fix: removed process.env.PORT

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
+app.use(express.json());
 // require('dotenv').config();
 
 app.get('/', (request, response) => {
     response.json({ message: 'Hello App' });
 });
 
-app.listen(process.env.PORT, () => {
-    console.log('🟠 SERVER STARTED ON PORT 5002');
+app.post('/courses', (request, response) => {
+    const { name } = request.body;
+    return response.json({ name });
+});
+
+app.listen(5002, () => {
+    console.log('🔶SERVER STARTED ON PORT 5002');
 });
