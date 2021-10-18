276d6873c922f8a44585dcb7abb25f65b3731b8e
elias fernandes <eliasfernandescout@gmail.com>
2021-10-17 22:59:19 -0300

    add eslint to project

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
 
-app.get('/', (request, response)=>{
-    response.json({message: 'Hello App'})
+app.get('/', (request, response) => {
+    response.json({ message: "Hello App" });
 });
 
+app.post("/", (request, response) => {});
 
-
-app.listen(process.env.PORT, ()=>{
-    console.log('🟠 SERVER STARTED ON PORT 5002')
-})
+app.listen(process.env.PORT, () => {
+    console.log("🟠 SERVER STARTED ON PORT 5002");
+});
