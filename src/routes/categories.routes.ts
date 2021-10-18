32914c1041bf98fa56cafc84869702d4c7aba3aa
elias fernandes <eliasfernandescout@gmail.com>
2021-10-18 18:54:22 -0300

    create a Category model and a get route

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,30 @@
 import { request, response, Router } from 'express';
+import { v4 as uuidV4 } from 'uuid';
+
+import { Category } from '../model/Category';
 
 const categoriesRoutes = Router();
+const categoriesList = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
-categoriesRoutes.post('/categories', (request, response) => {
+categoriesRoutes.post('/', (request, response) => {
     const { name, description } = request.body;
-    categories.push({
+
+    const category: Category = {
         name,
         description,
-    });
+        // id: uuidV4(),
+        created_at: new Date(),
+    };
+    categories.push(category);
 
-    return response.status(201).json({ name, description }).send();
+    return response.status(201).json({ category }).send();
 });
 
-export { categoriesRoutes };
+categoriesList.get('/', (request, response) => {
+    const listCategory: Category[] = categories;
+    return response.send({ listCategory });
+});
+
+export { categoriesRoutes, categoriesList };
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import express, { response } from 'express';
 
-import { categoriesRoutes } from './routes/categories.routes';
+import { categoriesRoutes, categoriesList } from './routes/categories.routes';
 
 const app = express();
 app.use(express.json());
 // require('dotenv').config();
 
-app.use(categoriesRoutes);
+app.use('/categories', categoriesRoutes);
+app.use('/categoriesList', categoriesList);
 
 app.listen(5002, () => {
     console.log('🔶SERVER STARTED ON PORT 5002');
