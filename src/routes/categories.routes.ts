0589be7487ac268eee92f380c6c7ae1f761049fb
elias fernandes <eliasfernandescout@gmail.com>
2021-10-20 20:04:35 -0300

    freat: create a service to make new categories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -3,20 +3,19 @@ import { v4 as uuidV4 } from 'uuid';
 
 import { Category } from '../model/Category';
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
-const categories: Category[] = [];
-
 categoriesRoutes.post('/', (request, response) => {
     const { name, description } = request.body;
-    const categoryAlreadyExists = categoriesRepository.findByName(name);
-    if (categoryAlreadyExists) {
-        return response.status(400).json({ error: 'Category Already Exists' });
-    }
 
-    categoriesRepository.create({ name, description });
+    const createCategoryService = new CreateCategoryService(
+        categoriesRepository,
+    );
+
+    createCategoryService.execute({ name, description });
 
     return response.status(201).send();
 });
