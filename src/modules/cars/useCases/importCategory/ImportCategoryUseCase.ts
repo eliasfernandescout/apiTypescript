b97893d0edbbc02203a40f8f26af5b00b29cb8ec
elias fernandes <eliasfernandescout@gmail.com>
2021-10-24 16:24:51 -0300

    feat: create parsefile method

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -6,6 +6,10 @@ class ImportCategoryUseCase {
         const stream = fs.createReadStream(file.path);
         const parseFile = csvParse({});
         stream.pipe(parseFile);
+
+        parseFile.on('data', async line => {
+            console.log(line);
+        });
     }
 }
 
