711b3e46b739c0cdd11f3673a2dd3c161caea706
elias fernandes <eliasfernandescout@gmail.com>
2021-10-26 00:04:56 -0300

    feat: add swagger documentation

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import express, { response } from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import { router } from './routes';
 import { categoriesRoutes } from './routes/categories.routes';
 import { specificationsRoutes } from './routes/specifications.routes';
+import swaggerFile from './swagger.json';
 
 const app = express();
 app.use(express.json());
-// require('dotenv').config();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.use(router);
 
 app.listen(5002, () => {
