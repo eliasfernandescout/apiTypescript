bbf3b40ec64f2439d6da3e585fce25c62a3ee9c1
elias fernandes <eliasfernandescout@gmail.com>
2021-12-27 18:12:43 -0300

    feat: add createUserUseCase and IUsersRepository singleton container

--- src/modules/accounts/usecases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/usecases/createUser/CreateUserUseCase.ts
@@ -1,1 +1,29 @@
-console.log('TESTTE')
+import { inject, injectable } from 'tsyringe';
+
+import { ICreateUsersDTO } from '../../dtos/ICreateUserDTO';
+import { IUsersRepository } from '../../repositories/IUsersRepositories';
+
+@injectable()
+class CreateUserUseCase {
+    constructor(
+        @inject('UsersRepository')
+        private usersRepository: IUsersRepository,
+    ) {}
+    async execute({
+        name,
+        username,
+        email,
+        password,
+        driver_license,
+    }: ICreateUsersDTO): Promise<void> {
+        await this.usersRepository.create({
+            name,
+            username,
+            email,
+            password,
+            driver_license,
+        });
+    }
+}
+
+export { CreateUserUseCase };
