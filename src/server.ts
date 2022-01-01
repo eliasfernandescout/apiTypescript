add4f9330327f19e15ecb2986df273d10a5d583f
elias fernandes <eliasfernandescout@gmail.com>
2022-01-01 16:47:25 -0300

    feat: add app.use(error) and express async errors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
-import express, { response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
 import './database';
 import './shared/container';
+import { AppError } from './errors/AppError';
 import { router } from './routes';
 import swaggerFile from './swagger.json';
 
@@ -12,6 +14,20 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+app.use(
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+        if (err instanceof AppError) {
+            return response.status(err.statusCode).json({
+                message: err.message,
+            });
+        }
+        return response.status(500).json({
+            status: 'error',
+            message: `Internal server error - ${err.message} `,
+        });
+    },
+);
 
 app.listen(5002, () => {
     console.log('🟣SERVER STARTED ON PORT 5002');
