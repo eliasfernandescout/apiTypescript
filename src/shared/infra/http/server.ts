e3a223e254b6e1baa8331d03aba2cf509cc98251
elias fernandes <eliasfernandescout@gmail.com>
2022-01-02 17:18:51 -0300

    refactor: move server.ts for shared/infra/http

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,11 +2,11 @@ import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
-import './database';
-import './shared/container';
-import { AppError } from './errors/AppError';
-import { router } from './shared/infra/http/routes';
-import swaggerFile from './swagger.json';
+import '../../../database';
+import '../../container';
+import { AppError } from '../../../errors/AppError';
+import swaggerFile from '../../../swagger.json';
+import { router } from './routes';
 
 const app = express();
 app.use(express.json());
