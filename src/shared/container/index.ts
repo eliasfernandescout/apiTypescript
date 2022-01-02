c0c2c4d6030e0be2d033c3d060c6078ef1a0350e
elias fernandes <eliasfernandescout@gmail.com>
2022-01-02 17:33:27 -0300

    refactor: update import of ICategoriesRepository from shared/container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,9 +2,9 @@ import { container } from 'tsyringe';
 
 import { UsersRepository } from '../../modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepositories';
-import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
 import { CategoriesRepository } from '../../modules/cars/infra/typeorm/repositories/CategoriesRepository';
 import { SpecificationRepository } from '../../modules/cars/infra/typeorm/repositories/SpecificationsRepository';
+import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
 import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
 // ICategoriesRepository
 container.registerSingleton<ICategoriesRepository>(
